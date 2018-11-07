81952648b7dbe5809a2ca890d956cde5c19ecb6e
Parush Garg <parush.theultimate@gmail.com>
2018-11-07 16:44:39 -0500

    First UI commit

---
+++ UI/sign-up-login-form/js/index.js
@@ -0,0 +1,69 @@
+function validate()
+{
+    if(document.getElementById("email").value == "workshop"
+       && document.getElementById("pwd").value == "workshop" )
+    {
+        alert( "validation succeeded" );
+        //location.href="run.html";
+    }
+    else
+    {
+        //alert( "validation failed" );
+        location.href="../multiple-file-uploader/admin.html";
+    }
+}
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
